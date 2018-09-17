9bfa0aa96b219a7ab9e83700670b176d59140ab0
jManock <jonmanock@gmail.com>
2018-09-17 09:05:37 -0400

    hope this will work

---
+++ reactphoto/src/CheckButton.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class CheckButton extends React.Component{
+  
+}
---
+++ reactphoto/src/Image.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import CheckButton from './CheckButton';
+
+export default class Image extends React.Component{
+  
+}
