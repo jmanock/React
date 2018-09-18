3b39b9a72d5ec3cb4a1d448a848d4798971d76de
jManock <jonmanock@gmail.com>
2018-09-18 15:50:36 -0400

    going to work on this one

--- reactphoto/src/index.js
+++ reactphoto/src/index.js
@@ -1,128 +1,147 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var PhotoGallery = React.createClass({
-  getInitialState:function(){
-    return {fullScreenPhotoIndex:-1};
+const app = document.getElementById('root');
+const data = [
+  {
+    id:1,
+    name:'Island',
+    image:'https://images.unsplash.com/photo-1442530792250-81629236fe54?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=9631adb2d2f752e3a0734f393fef634b'
   },
-
-  handleFullPhotoClick:function(){
-    this.setState({fullScreenPhotoIndex:-1});
+  {
+    id:2,
+    name:'Forest',
+    image:'https://images.unsplash.com/photo-1468851508491-4f854ec88aa0?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=b1222b6a1d3694cac76d2a23c3a02254'
   },
-
-  handlePhotoClick:function(photoIndex){
-    this.setState({fullScreenPhotoIndex:photoIndex});
+  {
+    id:3,
+    name:'whale',
+    image:'https://images.unsplash.com/photo-1454991727061-be514eae86f7?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=3c55430f01fe9ac9a9ccb3383d1416ff'
   },
-
-  handleBlockClick:function(blockStartIndex,cellIndex){
-    this.handlePhotoClick(blockStartIndex + cellIndex);
+  {
+    id:4,
+    name:'Boat',
+    image:'https://images.unsplash.com/photo-1467890947394-8171244e5410?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=9396f0adf263b51b44626228225684d0'
   },
-
-  handlePhotoNavClick:function(delta,evt){
-    var newImageIndex = this.state.fullScreenPhotoIndex + delta;
-    if(newImageIndex < 0){
-      newImageIndex = this.props.photoURLs.length -1;
-    }else if(newImageIndex >= this.props.photoURLs.length){
-      newImageIndex = 0;
-    }
-    this.setState({fullScreenPhotoIndex:newImageIndex});
-    evt.preventDefault();
-    evt.stopPropagation();
+  {
+    id:5,
+    name:'Mountain',
+    image:'https://images.unsplash.com/photo-1443302382600-0bfacc473376?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=0c0f26518c1001f67b6c2e4480a8d3e0'
   },
+  {
+    id:6,
+    name:'Floweres',
+    image:'https://images.unsplash.com/photo-1429091443922-e7d9ae79a837?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=e81cb6a60c53788559edb9bec21b80fc'
+  },
+  {
+    id:7,
+    name:'Fire',
+    image:'https://images.unsplash.com/photo-1468245856972-a0333f3f8293?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=1f57cc13084e32839627453821a43abf'
+  },
+  {
+    id:8,
+    name:'garden',
+    image:'https://images.unsplash.com/photo-1427392797425-39090deb14ec?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=8bfe49466d0da200e61128a8ab0e8fbe'
+  },
+  {
+    id:9,
+    name:'ridge',
+    image:'https://images.unsplash.com/photo-1445723356089-6dbb51d9c4f8?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=6e476c6e7ce1adac161295616d1bec05'
+  }
+];
 
-  render:function(){
-    var photoBlocks = [];
-    var numImages = this.props.photURLs.length;
-    var currentImageIndex = 0;
-
-    while(currentImagesIndex < numImages){
-      var photoBlockImages = [];
-      var photoBlockClickCallback = this.handleBlockClick.bind(this, currentImageIndex);
-
-      var numImagesInBlock = this.getNumImagesInPhotoBlock(photoBlocks.length);
-      for(var i = 0; i < numImagesInBlock && currentImageIndex < numImages; i++){
-        photoBlockImages.push(this.props.photoURLs[currentImageIndex++]);
-      }
-      photoBlocks.push(<PhotoBlock images={photoBlockImages} onPhotoClick={photoBlockClickCallback} />);
-    }
-
-    var fullPhotoURL = null;
-    var fullPhotoImageIndex = this.state.fullScreenPhotoIndex;
-    if(fullPhotoImageIndex >= 0 && fullPhotoImageIndex < numImages){
-      fullPhotoURL = this.props.photoURLs[fullPhotoImageIndex];
-    }
+class App extends React.Component{
+  render(){
     return(
-      <div className='photGallery'>
-        {photoBlocks}
-        <FullPhoto photoURL={fullPhotoURL} onClick={this.handleFullPhotoClick}>
-          <PhotoNav className='photoNav leftArrow' onClick={this.handlePhotoNavClick.bind(this,-1)} />
-          <PhotoNave className='photoNav rightArrow' onClick={this.handlePhotoClick.bind(this,1)} />
-        </FullPhoto>
+      <Tiles data={this.props.data} />
+    );
+  }
+}
+
+class Tiles extends React.Component{
+  render(){
+    return(
+      <div className='tiles'>
+        {this.props.data.map((data) => {
+          return <Tile data={data} key={data.id} />
+        })}
       </div>
     );
-  },
-
-  getNumImagesInPhotoBlock:function(blockIndex){
-    var layoutConfig = [1,2,3,4];
-    return layoutConfig[blockIndex % layoutConfig.length];
   }
-});
+}
 
-var PhotoBlock = React.createClass({
-  render:function(){
-    var numImages = this.props.images.length;
-    if(numImages == 1){
-      return(
-        <div className='photoBlock'>
-          <Photo onClick={this.props.onPhotoClick.bind(this,0)} className='cell_1' photoURL={this.props.images[0]} />
-        </div>
-      );
-    }else if(numImages == 2){
-      <div className='photoBlock'>
-        <Photo onClick={this.props.onPhotoClick.bind(this,0)} className='cell_2h' photoURL={this.props.images[0]} />
-        <Photo onClick={this.props.onPhotoClick.bind(this,1)} className='cell_2h' photoURL={this.props.images[1]} />
-      </div>
-    }else if(numImages === 3){
-      <div className='photoBlock'>
-        <Photo onClick={this.props.onPhotoClick.bind(this,0)} className='cell_2v' photoURL={this.props.images[0]} />
-        <Photo onClick={this.props.onPhotoClick.bind(this,1)} className='cell_4' photoURL={this.props.images[1]} />
-        <Photo onClick={this.props.onPhotoClick.bind(this,2)} className='cell_4' photoURL={this.props.images[2]} />
-      </div>
-    }else{
-      return(
-        <div className='photoBlock'>
-          <Photo onClick={this.props.onPhotoClick.bind(this,0)} className='cell_4' photoURL={this.props.images[0]} />
-          <Photo onClick={this.props.onPhotoClick.bind(this,1)} className='cell_4' photoURL={this.props.images[1]} />
-          <Photo onClick={this.props.onPhotoClick.bind(this,2)} className='cell_4' photoURL={this.props.images[2]} />
-          <Photo onClick={this.props.onPhotoClick.bind(this,3)} className='cell_4' photoURL={this.props.images[3]} />
-        </div>
-      );
-    }
-  }
-});
-
-var Photo = React.createClass({
-  render:function(){
-    var divStyle = {
-      backgroundImage:'url('+this.props.photoURL +')'
+class Tile extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      open:false,
+      mouseOver:false
     };
-    return(
-      <div className={'photoPreview'+this.props.className} style={divStyle} onClick={this.props.onClick} />
-    );
+    this._clickHandler = this._clickHandler.bind(this);
+    this._mouseEnter = this._mouseEnter.bind(this);
+    this._mouseLeave = this._mouseLeave.bind(this);
   }
-});
 
-var FullPhoto = React.createClass({
-  render:function(){
-    if(!this.props.photURL){
-      return(<div />);
+  _mouseEnter(e){
+    e.preventDefault();
+    if(this.state.mouseOver === false){
+      this.setState({
+        mouseOver:true
+      });
     }
+  }
+
+  _mouseLeave(e){
+    e.preventDefault();
+    if(this.state.mouseOver === true){
+      this.setState({
+        mouseOver:false
+      });
+    }
+  }
+
+  _clickHandler(e){
+    e.preventDefault();
+    if(this.state.open === false){
+      this.setState({
+        open:true
+      });
+    }else{
+      this.setState({
+        open:false
+      });
+    }
+  }
+
+  render(){
+    let tileStyle = {};
+
+    if(this.state.open){
+      tileStyle = {
+        width:'62vw',
+        height:'62vw',
+        position:'absolute',
+        top:'50%',
+        left:'50%',
+        margin:0,
+        marginTop:'-31vw',
+        marginLeft:'-31vw',
+        boxShadow:'0 0 40px 5px rgba(0,0,0,0.3)',
+        transform:'none'
+      };
+    }else{
+      tileStyle = {
+        width:400,
+        height:400
+      };
+    }
+
     return(
-      <div className='fullPhoto' onClick={this.props.onClick}>
-        <img src={this.props.photoURL} /> {this.props.children}
+      <div className='tile'>
+        <img onMouseEnter={this._mouseEnter} onMouseLeave={this._mouseLeave} onClick={this._clickHandler} src={this.props.data.image} alt={this.props.data.name} style={tileStyle} />
       </div>
     );
   }
-});
+}
 
-var 
+ReactDOM.render(<App data={data} />, app);
