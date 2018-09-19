2a13c596ccf985ebe221971c1df4e2ec0936913b
jManock <jonmanock@gmail.com>
2018-09-19 09:17:39 -0400

    needs style

---
+++ reactphoto/src/images.js
@@ -0,0 +1,237 @@
+export const data = [
+    {
+      id:1,
+      name:'Locks next to the Berlin Wall',
+      image:require('./images/1.jpg')
+    },
+    {
+      id:2,
+      name:'Berlin Wall',
+      image:require('./images/2.jpg')
+    },
+    {
+      id:3,
+      name:'Fore Aces yacht',
+      image:require('./images/3.jpg')
+    },
+    {
+      id:4,
+      name:'Brit and "G.P." waiting for dinner at DiMillos',
+      image:require('./images/4.jpg')
+    },
+    {
+      id:5,
+      name:'Lobster Mac & Cheese at DiMillos',
+      image:require('./images/5.jpg')
+    },
+    {
+      id:6,
+      name:'Lobster Mac & Cheese at DiMillos',
+      image:require('./images/6.jpg')
+    },
+    {
+      id:7,
+      name:'Beautiful view in New Harbor',
+      image:require('./images/7.jpg')
+    },
+    {
+      id:8,
+      name:'Ocean side mansions',
+      image:require('./images/8.jpg')
+    },
+    {
+      id:9,
+      name:'Calm waters',
+      image:require('./images/9.jpg')
+    },
+    {
+      id:10,
+      name:'Another view',
+      image:require('./images/10.jpg')
+    },
+    {
+      id:11,
+      name:'Ship off in the distance',
+      image:require('./images/11.jpg')
+    },
+    {
+      id:12,
+      name:'Person fishing',
+      image:require('./images/12.jpg')
+    },
+    {
+      id:13,
+      name:'Ship atleast 20 miles out',
+      image:require('./images/13.jpg')
+    },
+    {
+      id:14,
+      name:'A replica of the last wooden ship built, flags are 144 feet high',
+      image:require('./images/14.jpg')
+    },
+    {
+      id:15,
+      name:'Lobster mobile',
+      image:require('./images/15.jpg')
+    },
+    {
+      id:16,
+      name:'Closest Brit will get to seafood',
+      image:require('./images/16.jpg')
+    },
+    {
+      id:17,
+      name:'Lobster trap chairs',
+      image:require('./images/17.jpg')
+    },
+    {
+      id:18,
+      name:'Adult size lobster traps',
+      image:require('./images/18.jpg')
+    },
+    {
+      id:19,
+      name:'First lobster roll at the Sea Basket',
+      image:require('./images/19.jpg')
+    },
+    {
+      id:20,
+      name:'A different looking boat',
+      image:require('./images/20.jpg')
+    },
+    {
+      id:21,
+      name:'Clear view of Mohhegan Island',
+      image:require('./images/21.jpg')
+    },
+    {
+      id:22,
+      name:'Another view',
+      image:require('./images/22.jpg')
+    },
+    {
+      id:23,
+      name:'Reading the stock reports',
+      image:require('./images/23.jpg')
+    },
+    {
+      id:24,
+      name:'Burger, brown bread, and beans',
+      image:require('./images/24.jpg')
+    },
+    {
+      id:25,
+      name:'Lobster bake meal: lobster, corn, potatos, clams, and onion',
+      image:require('./images/25.jpg')
+    },
+    {
+      id:26,
+      name:'Lobster bake desert: molass cookies, blueberry crum cake and fudge brownie',
+      image:require('./images/26.jpg')
+    },
+    {
+      id:27,
+      name:'"G.P." napping after the lobster bake',
+      image:require('./images/27.jpg')
+    },
+    {
+      id:28,
+      name:'Pemaquid Point Lighthouse',
+      image:require('./images/28.jpg')
+    },
+    {
+      id:29,
+      name:'Grilling out a T-Bone',
+      image:require('./images/29.jpg')
+    },
+    {
+      id:30,
+      name:'Finished stake',
+      image:require('./images/30.jpg')
+    },
+    {
+      id:31,
+      name:'Steak, grilled potato, and brown bread',
+      image:require('./images/31.jpg')
+    },
+    {
+      id:32,
+      name:'Nice view of the cove and the neighbors house',
+      image:require('./images/32.jpg')
+    },
+    {
+      id:33,
+      name:'Line for Reds Eats',
+      image:require('./images/33.jpg')
+    },
+    {
+      id:34,
+      name:'Keep going',
+      image:require('./images/34.jpg')
+    },
+    {
+      id:35,
+      name:'Nap at the state mesuem',
+      image:require('./images/35.jpg')
+    },
+    {
+      id:36,
+      name:'Maine state Capital',
+      image:require('./images/36.jpg')
+    },
+    {
+      id:37,
+      name:'Fog rolling in, looks like the island is floating',
+      image:require('./images/37.jpg')
+    },
+    {
+      id:38,
+      name:'Another shot',
+      image:require('./images/38.jpg')
+    },
+    {
+      id:39,
+      name:'Can not even see the ocean from the deck',
+      image:require('./images/39.jpg')
+    },
+    {
+      id:40,
+      name:'Fog behind the house',
+      image:require('./images/40.jpg')
+    },
+    {
+      id:41,
+      name:'View of the hardy boat at Shaws',
+      image:require('./images/41.jpg')
+    },
+    {
+      id:42,
+      name:'Lobster roll at Shaws',
+      image:require('./images/42.jpg')
+    },
+    {
+      id:43,
+      name:'Brit on the deck with Monheagan in the background',
+      image:require('./images/43.jpg')
+    },
+    {
+      id:44,
+      name:'There is a person sitting in the seat',
+      image:require('./images/44.jpg')
+    },
+    {
+      id:45,
+      name:'Lobster eating the building',
+      image:require('./images/45.jpg')
+    },
+    {
+      id:46,
+      name:'Biggest crain in the world',
+      image:require('./images/46.jpg')
+    },
+    {
+      id:47,
+      name:'Last meal Logan airport "Walburgers"',
+      image:require('./images/47.jpg')
+    }
+  ];
