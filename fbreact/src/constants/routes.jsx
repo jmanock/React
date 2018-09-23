9174fd9b04bbb86f3ec5f50cc1ea9237efa132b7
jManock <jonmanock@gmail.com>
2018-09-23 12:08:53 -0400

    seting up fb

--- fbreact/src/constants/routes.jsx
+++ fbreact/src/constants/routes.jsx
@@ -1,6 +1,7 @@
+export const ACCOUNT = '/account';
 export const SIGN_UP = '/signup';
-export const SIGN_IN = '/singin';
+export const SIGN_IN = '/signin';
 export const LANDING ='/';
 export const HOME ='/home';
-export const ACCOUNT = '/account';
+
 export const PASSWORD_FORGET = '/pw-forget';
