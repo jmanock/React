776389133c635c7d8dfb5f66c7e5eb49e10c7413
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2018-10-09 09:12:55 -0400

    done works

--- exampleredux/src/App.js
+++ exampleredux/src/App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import Posts from './Components/posts';
 import PostForm from './Components/postForm';
+import {Provider} from 'react-redux';
+import store from './Store';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PostForm />
-        <hr />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
--- exampleredux/src/Components/posts.jsx
+++ exampleredux/src/Components/posts.jsx
@@ -1,24 +1,25 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../Actions/postActions';
+import PropTypes from 'prop-types';
 
 class Posts extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      posts:[]
-    };
-  }
   componentWillMount(){
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data => this.setState({posts:data}));
+    this.props.fetchPosts();
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(nextProps.newPost){
+      this.props.posts.unshift(nextProps.newPost);
+    }
   }
   render(){
-    const postItem = this.state.posts.map(post => (
+    const postItem = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
       </div>
-    ))
+    ));
     return(
       <div>
         <h1>Posts</h1>
@@ -27,4 +28,16 @@ class Posts extends Component{
     )
   }
 }
-export default Posts;
+
+Posts.propTypes = {
+  fetchPosts:PropTypes.func.isRequired,
+  posts:PropTypes.array.isRequired,
+  newPost:PropTypes.object
+}
+
+const mapStateToProps = state => ({
+  posts: state.posts.items,
+  newPost:state.posts.item
+});
+
+export default connect(mapStateToProps, {fetchPosts})(Posts);
