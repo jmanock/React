da08440e91632df01179a6f1fe0c18687b622e18
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2018-10-11 13:09:47 -0400

    works and looks good

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
+import Todos from './Todos';
+import AddTodo from './AddForm';
 
 class App extends Component {
+  state = {
+    todos:[
+      {id:1, content:'buy some milks'},
+      {id:2, content:'play with winnie'}
+    ]
+  }
+  deleteTodo = (id) =>{
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos:todos
+    })
+  }
+  addTodo = (todo) =>{
+    todo.id = Math.random();
+    let todos = [...this.state.todos,todo];
+    this.setState({
+      todos
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+        <h1 className='center blue-text'>Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
