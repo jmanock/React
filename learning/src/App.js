5ea79aebb1400d56249272e6691805922393f02f
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2018-10-11 08:34:42 -0400

    working threw tuts

--- learning/src/App.js
+++ learning/src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import Testing from './test';
 
 class App extends Component {
   render() {
-    return (
-      <Testing />
+    return (<h1>Hi Friend</h1>
     );
   }
 }
